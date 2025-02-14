2caf369ddc916b5f758d8d37f57d05e18e31c9fb
Mateusz Paprocki <mattpap@gmail.com>
2025-02-14 20:43:29 +0100

    Add `Logo` model to the public API

--- src/bokeh/models/tools.pyi
+++ src/bokeh/models/tools.pyi
@@ -31,7 +31,7 @@ from ..core.enums import (
     AutoType as Auto,
     DimensionsType as Dimensions,
     DimensionType as Dimension,
-    LogoStyleType as LogoStyle,
+    LogoVariantType as LogoVariant,
     PanDirectionType as PanDirection,
     RegionSelectionModeType as RegionSelectionMode,
     SelectionModeType as SelectionMode,
@@ -150,7 +150,12 @@ class InspectTool(GestureTool):
 
     toggleable: bool = ...
 
-@dataclass()
+@dataclass
+class Logo(UIElement):
+
+      variant: LogoVariant = ...
+
+@dataclass
 class ToolButton(UIElement):
 
     tool: Tool | ToolProxy = ...
@@ -166,7 +171,7 @@ class Toolbar(UIElement):
 
     children: Auto | list[UIElement | None] = ...
 
-    logo: LogoStyle | None = ...
+    logo: LogoVariant | None = ...
 
     autohide: bool = ...
 
